df1ba920d269c3fe64faee20d675beabcd443011
FIL3SAV3D <fil3sav3d@gmail.com>
2024-04-21 17:22:24 +0200

    .vs Ignore
    Forgot to Ignore the .vs files

--- PasicEditor/Editors/WorldEditor/ProjectLayoutView.xaml.cs
+++ PasicEditor/Editors/WorldEditor/ProjectLayoutView.xaml.cs
@@ -36,8 +36,17 @@ namespace PasicEditor.Editors
 
         private void OnGameEntities_ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var entity = (sender as ListBox).SelectedItems[0];
-            GameEntityView.Instance.DataContext = entity;
+            GameEntityView.Instance.DataContext = null;
+            var listBox = sender as ListBox;
+            if (e.AddedItems.Count > 0)
+            {
+                GameEntityView.Instance.DataContext = listBox.SelectedItems[0];
+            }
+
+            var newSelection = listBox.SelectedItems.Cast<GameEntity>().ToList();
+            var previousSelection = newSelection.Except(e.AddedItems.Cast<GameEntity>()).Concat(e.RemovedItems.Cast<GameEntity>()).ToList();
+
+
         }
     }
 }
