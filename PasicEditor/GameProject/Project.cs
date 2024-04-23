8b28739fe56eafb4d2ac531320a2dbcc6186accc
FIL3SAV3D <fil3sav3d@gmail.com>
2024-04-23 20:39:58 +0200

    Multiselect Completed. Game Engine Started
    I was going to commit before I started on the game engine but here i am. Damn it

--- PasicEditor/GameProject/Project.cs
+++ PasicEditor/GameProject/Project.cs
@@ -14,7 +14,7 @@ using System.Windows.Input;
 namespace PasicEditor.GameProject
 {
     [DataContract(Name = "Game")]
-    public class Project : ViewModelBase
+    class Project : ViewModelBase
     {
         public static string Extension { get; } = ".pasic";
         [DataMember]
